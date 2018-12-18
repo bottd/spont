83ccb320d84435bf7b32b6e0c88be649c4920aeb
Graham <g@masterofnone.com>
2018-12-18 14:39:57 -0700

    Add background location package

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,26 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import { StatusBar, StyleSheet, View } from "react-native";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import BackgroundGeolocation from "react-native-background-geolocation";
+
+
 
 import DarkStyles from '../../styles/DarkStyles.json';
 
-export default () => (
-  <View style={styles.container}>
-  <StatusBar barStyle="light-content"/>
-    <MapView
-      provider={ PROVIDER_GOOGLE }
-      style={ styles.container }
-      customMapStyle={ DarkStyles }
-      showsUserLocation = {true}
-      initialRegion={{
-        latitude: 39.7392,
-        longitude: -104.9953,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}
-    />
-  </View>
-);
+export default class App extends Component {
+  constructor() {
+    super()
+  }
+
+  componentDidMount() {
+    
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+      <StatusBar barStyle="light-content"/>
+        <MapView
+          provider={ PROVIDER_GOOGLE }
+          style={ styles.container }
+          customMapStyle={ DarkStyles }
+          showsUserLocation = {true}
+          initialRegion={{
+            latitude: 39.7392,
+            longitude: -104.9953,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </View>
+    )
+  }
+}
+
+
 
 const styles = StyleSheet.create({
     container: {
