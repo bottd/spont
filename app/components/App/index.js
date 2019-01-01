ec8f0289ba6f37477cc794115e1a8257ec7c5f78
Graham <g@masterofnone.com>
2019-01-01 11:36:03 -0700

    Revise app to accomodate location and motion logging

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -10,53 +10,28 @@ export default class App extends Component {
   constructor() {
     super()
     this.state = {
-      user: '',
-      position: {
-        latitude: 0,
-        longitude: 0,
-      }
+      user: ''
     }
   }
 
-  async componentDidMount() {
+  async componentWillMount() {
     await this.linkUser()
+    BackgroundGeolocation.onLocation(this.onLocation, this.onError);
+    BackgroundGeolocation.onMotionChange(this.onMotionChange);
 
-    BackgroundGeolocation.onMotionChange(this.logLocation);
-
-    BackgroundGeolocation.onMotionChange((event) => {
-      if (event.isMoving) {
-         console.log('[onMotionChange] Device has just started MOVING ', event.location);
-      } else {
-         console.log('[onMotionChange] Device has just STOPPED:  ', event.location);
-      }
-    });
-    
-
-    // BackgroundGeolocation.onLocation(this.onLocation, this.onError);
     BackgroundGeolocation.ready({
-    
-      // Geolocation Config
-      // reset: true,
+      reset: true,
+      disableStopDetection: true,
       desiredAccuracy: BackgroundGeolocation.DESIRED_ACCURACY_HIGH,
       distanceFilter: 10,
-      // Activity Recognition
-      stopTimeout: 5,
-      // Application config
-      debug: true, // <-- enable this hear sounds for background-geolocation life-cycle.
+      stopTimeout: 1,
+      debug: true, 
       logLevel: BackgroundGeolocation.LOG_LEVEL_VERBOSE,
-      stopOnTerminate: false,  
-      startOnBoot: true, 
-      // url: 'http://yourserver.com/locations',
-      // batchSync: false,       // <-- [Default: false] Set true to sync locations to server in a single HTTP request.
-      // autoSync: false,         // <-- [Default: true] Set true to sync each location to server as it arrives.
-      // headers: {              // <-- Optional HTTP headers
-      //   "X-FOO": "bar"
-      // },
-      // params: {               // <-- Optional HTTP params
-      //   "auth_token": "maybe_your_server_authenticates_via_token_YES?"
-      // }
+      stopOnTerminate: false,
+      startOnBoot: true
     }, (state) => {
       console.log("- BackgroundGeolocation is configured and ready: ", state.enabled);
+
       if (!state.enabled) {
         BackgroundGeolocation.start(function() {
           console.log("- Start success");
@@ -64,7 +39,7 @@ export default class App extends Component {
       }
     });
   }
-
+  
   linkUser = async () => {
     const { user } = this.state;
     if (!user) {
@@ -72,36 +47,37 @@ export default class App extends Component {
       this.setState( { user } )
     }
   }
-
-  logLocation = event => {
-    if (!event.isMoving) {
-      const position = {
-        latitude: event.location.coords.latitude,
-        longitude: event.location.coords.longitude,
-      }
-      this.setState({ position } );
+  
+  onLocation = location => {
+    const { latitude, longitude } = location.coords;
+    if (location.coords.speed === 0) {
+      const position = { latitude, longitude }
       API.logCoords(this.state.user, position);
-    } else {
-      console.log('[onMotionChange] Device has just started MOVING ', event.location);
     }
   }
-
+  
+  onError = error => {
+    return
+  }
+  
+  onMotionChange = event => {
+    const { latitude, longitude } = event.location.coords;
+    if (!event.isMoving) {
+      const position = { latitude, longitude }
+      API.logCoords(this.state.user, position);
+    }
+  }
+  
   componentWillUnmount() {
     BackgroundGeolocation.removeListeners();
   }
 
   render() {
-    const { position, user } = this.state;
+    const { user } = this.state;
     return(
       <View >
         <StatusBar barStyle="light-content"/>
         <Map user={user}/>
-        <View>
-          <Text>{user}</Text>
-        </View>
-        <View>
-          <Text>{`LAT: ${position.latitude} LONG: ${position.longitude}`}</Text>
-        </View>
       </View>
     )
   }
