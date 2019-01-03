c9bdf003f95d8ef3e8aa897a51915c830a89f21d
Graham <g@masterofnone.com>
2019-01-03 10:36:08 -0700

    Add setup test file to mock device storage and react native maps

--- app/_tests_/setup.js
+++ app/_tests_/setup.js
@@ -1,3 +1,5 @@
+// import React from 'react';
+
 jest.mock('react-native', () => {
     let items = {};
 
@@ -30,6 +32,36 @@ jest.mock('react-native', () => {
                 items = {}
                 return Promise.resolve()
             }))
-        }
+        } 
     }
 });
+
+jest.mock('react-native-maps', () => {                                           
+  const React = require.requireActual('react');                                  
+  const MapView = require.requireActual('react-native-maps');                    
+                                                                                 
+  class MockCallout extends React.Component {                                    
+    render() {                                                                   
+      return React.createElement('Callout', this.props, this.props.children);    
+    }                                                                            
+  }                                                                              
+                                                                                 
+  class MockMarker extends React.Component {                                     
+    render() {                                                                   
+      return React.createElement('Marker', this.props, this.props.children);     
+    }                                                                            
+  }                                                                              
+                                                                                 
+  class MockMapView extends React.Component {                                    
+    render() {                                                                   
+      return React.createElement('MapView', this.props, this.props.children);    
+    }                                                                            
+  }                                                                              
+                                                                                 
+  MockCallout.propTypes = MapView.Callout.propTypes;                             
+  MockMarker.propTypes = MapView.Marker.propTypes;                               
+  MockMapView.propTypes = MapView.propTypes;                                     
+  MockMapView.Marker = MockMarker;                                               
+  MockMapView.Callout = MockCallout;                                             
+  return MockMapView;                                                            
+}); 
